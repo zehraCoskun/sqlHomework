86bd28b6189e31033d83c764766c2b71e3517b97
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2022-12-29 05:45:15 +0300

    Update Homework4.sql

--- Homework4.sql
+++ Homework4.sql
@@ -1,1 +1,13 @@
-
+1-
+select distinct replacement_cost from film;
+2-
+select count (distinct replacement_cost) from film;
+3-
+select count (*) from film
+where title like 'T%';
+4-
+select count (*) from country
+where country like '_____';
+5-
+select count (*) from city
+where city ilike '%r';
