bd79d6db2881c8bdc3aa1d655dd1a897407e0568
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2022-12-29 17:43:49 +0300

    Create Homework7.sql

---
+++ Homework7.sql
@@ -0,0 +1,19 @@
+
+1-
+select rating from film
+group by rating;
+
+2-
+select replacement_cost,count(*) from film
+group by replacement_cost
+having count(*)>50;
+
+3-
+select store_id , count(*)from customer
+group by store_id;
+
+4-
+select country_id, count(*) from city
+group by country_id
+order by 2 desc
+limit 1;
