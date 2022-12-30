d3fc99af73ecfc7f5f2153874055362bd0919dca
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2022-12-30 19:48:21 +0300

    Update homework9.sql

--- homework9.sql
+++ homework9.sql
@@ -1,1 +1,11 @@
-
+1-
+select city.city, country.country from city
+join country on city.country_id = country.country_id;
+2-
+select customer.first_name, customer.last_name, payment_id from customer
+inner join payment
+on customer.customer_id = payment.customer_id;
+3-
+select rental_id, first_name, last_name from customer
+inner join rental
+on customer.customer_id = rental.customer_id;
