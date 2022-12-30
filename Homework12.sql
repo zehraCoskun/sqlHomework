9dd571599533f517f473250f434df7c3e962c35d
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2022-12-31 01:01:34 +0300

    Update Homework12.sql

--- Homework12.sql
+++ Homework12.sql
@@ -1,21 +1,21 @@
 1-
 select count(*) from film
 where length>
-(
-	select avg(length) from film
-);
+(select avg(length) from film);
+
 2-
 select count(*) from film
 where rental_rate=
 (select max(rental_rate) from film);
+
 3-
 select title from film 
 where rental_rate=
 (select min(rental_rate) from film)
-and
-replacement_cost =
+and replacement_cost =
 (select min(replacement_cost)from film);
+
 4-
-select customer_id from payment
-where amount =
-(select max(amount) from payment);
+select payment.customer_id, first_name, last_name from payment
+inner join customer on customer.customer_id=payment.customer_id
+where amount =(select max(amount) from payment);
